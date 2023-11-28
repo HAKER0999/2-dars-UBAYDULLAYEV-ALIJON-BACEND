6ebe40117d8c2dc93377596d6510a70808e9de81
ALIJON0999 <ubaydullayevalijon4@gmail.com>
2023-11-28 15:56:03 +0500

    first commit

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-# # x= int(input("x="))
-# # y= int(input("y="))
-# # print("JAVOB=",x * y)
-#
-# #2-vazifa
-# x= int(input("sonni kiriting:"))
-# y= int(input("nechinchi darajaga oshirmoqchisiz:"))
-# print( "Javob:", x ** y)
-# #1-vazifa tugadi
-#
-# #2-vazifa
-# r= int(input("bo'linuvchini kiritng:"))
-# t= int(input("bo'luvchini kiriting:"))
-# print( "Qolodiq:", r % t)
-# #2-vazifa tugadi
-#
-# #3-vazifa
-# b= int(input("kvadratning tomonini kiritin:"))
-# print("yuzasi:", b * b)
-# print("peremetri:", b * 4)
-# #3-vazifa tugadi
+# x= int(input("x="))
+# y= int(input("y="))
+# print("JAVOB=",x * y)
+
+#2-vazifa
+x= int(input("sonni kiriting:"))
+y= int(input("nechinchi darajaga oshirmoqchisiz:"))
+print( "Javob:", x ** y)
+#1-vazifa tugadi
+
+#2-vazifa
+r= int(input("bo'linuvchini kiritng:"))
+t= int(input("bo'luvchini kiriting:"))
+print( "Qolodiq:", r % t)
+#2-vazifa tugadi
+
+#3-vazifa
+b= int(input("kvadratning tomonini kiritin:"))
+print("yuzasi:", b * b)
+print("peremetri:", b * 4)
+#3-vazifa tugadi
 
 #4-vazifa
 s= int(input("Diometrini kiriting:"))
